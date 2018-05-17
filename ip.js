a7161755d5b7bf68441b41a9c0949de982d34f46
Aleksandr Gornostal <al.gornostal@gmail.com>
2018-05-17 14:40:05 +0300

    changed things for the better

--- ip.js
+++ ip.js
@@ -1,11 +1,11 @@
 var request = require("async-request")
 
 async function myPublicIp() {
-    const resp = await request("http://ifconfig.me/all.json", {
+    const resp = await request("https://api.ipify.org?format=json", {
         headers: { Accept: "application/json" }
     })
-    console.log(resp)
-    return resp.ip_addr
+    const jsonBody = JSON.parse(resp.body)
+    return jsonBody.ip
 }
 
 exports.myPublicIp = myPublicIp
